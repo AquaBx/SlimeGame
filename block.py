9c48c4e897762e334e30acdf5a387431d89bd0f2
tchauvel <tom.chauvel@etudiant.univ-rennes1.fr>
2022-11-05 22:20:26 +0100

    update

--- block.py
+++ block.py
@@ -3,7 +3,8 @@ import pygame
 class Block :
     def __init__(self,x,y,w,h,solide,src):
         self.texture = pygame.transform.scale(pygame.image.load(src), (w, h))
-        self.rect = pygame.Rect(x,y,w,h)
+        self.position = pygame.Vector2(x,y)
+        self.taille = pygame.Vector2(w,h)
         self.solide = solide
     
     def draw(self,window) :
--- entity.py
+++ entity.py
@@ -2,7 +2,10 @@ from cmath import rect
 import pygame
 
 class Entity:
-    def __init__(self,x,y,w,h):
-        self.rect = pygame.Rect(x,y,w,h)
+    def __init__(self,x,y,w,h,src):
+        self.position = pygame.Vector2(x,y)
         self.vitesse = pygame.Vector2(0,0)
         self.acceleration = pygame.Vector2(0,0)
+		
+        self.taille = pygame.Vector2(w,h)
+        self.texture = pygame.transform.scale(pygame.image.load(src), (w, h))
