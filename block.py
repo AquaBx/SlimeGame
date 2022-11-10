5ae48369a1a0e36fc940ccf780a2942b4ad3315d
CelJos <celia.josso@orange.fr>
2022-11-10 09:48:39 +0100

    Modification du dossier Démo-camera

--- block.py
+++ block.py
@@ -1,4 +1,6 @@
 import pygame
+from config import Config
+from camera import *
 
 class Block :
     def __init__(self,x,y,w,h,solide,src):
@@ -6,6 +8,15 @@ class Block :
         self.position = pygame.Vector2(x,y)
         self.taille = pygame.Vector2(w,h)
         self.solide = solide
+
     
     def draw(self,window) :
         window.blit(self.texture,self.rect.topleft)
+    
+    def implement_blocks():
+        return [ Block(Config.BLOCK_SIZE*i,Config.WINDOW_H-Config.BLOCK_SIZE,Config.BLOCK_SIZE,Config.BLOCK_SIZE,True,f'Assets/Tileset/tileMain{2}.png') for i in range(1,20) ]
+    
+    def blit_blocks(blocks, window, x_cam, y_cam):
+        for block in blocks:
+            ncoord = ( block.position.x-x_cam+Config.WINDOW_W/2 , block.position.y-y_cam+Config.WINDOW_H/2 )
+            window.blit(block.texture,ncoord)
