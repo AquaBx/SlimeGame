b79be67ddb15fe9c078f6ad4f8a206a665b1dcb7
celiajosso <70684974+celiajosso@users.noreply.github.com>
2022-11-10 09:48:39 +0100

    Modification du dossier Démo-camera

--- entity.py
+++ entity.py
@@ -1,5 +1,7 @@
 from cmath import rect
 import pygame
+from config import *
+from camera import Camera
 
 class Entity:
     def __init__(self,x,y,w,h,src):
@@ -9,3 +11,35 @@ class Entity:
 		
         self.taille = pygame.Vector2(w,h)
         self.texture = pygame.transform.scale(pygame.image.load(src), (w, h))
+    
+    def implement_player():
+        return Entity(10*Config.BLOCK_SIZE,Config.WINDOW_H-2*Config.BLOCK_SIZE,Config.BLOCK_SIZE,Config.BLOCK_SIZE,"Assets/GreenSlime/Grn_Idle1.png")
+
+    
+    def move(self):
+        keys_pressed = pygame.key.get_pressed()
+
+        self.acceleration.x = - self.vitesse.x*1000/Config.BLOCK_SIZE
+        self.acceleration.y = - self.vitesse.y*1000/Config.BLOCK_SIZE
+
+        if keys_pressed[pygame.K_d]:
+            self.acceleration.x += 2*Config.BLOCK_SIZE/Config.dt
+
+        if keys_pressed[pygame.K_q]:
+            self.acceleration.x -= 2*Config.BLOCK_SIZE/Config.dt
+
+        if keys_pressed[pygame.K_z]:
+            self.acceleration.y -= 2*Config.BLOCK_SIZE/Config.dt
+
+        if keys_pressed[pygame.K_s]:
+            self.acceleration.y += 2*Config.BLOCK_SIZE/Config.dt
+
+        self.vitesse.y += self.acceleration.y*Config.dt
+        self.position.y += self.vitesse.y*Config.dt
+
+        self.vitesse.x += self.acceleration.x*Config.dt
+        self.position.x += self.vitesse.x*Config.dt
+
+    def blit_player(self, window, x_cam, y_cam):
+        ncoord = ( self.position.x-x_cam+Config.WINDOW_W/2-self.taille.x/2 , self.position.y-y_cam+Config.WINDOW_H/2)
+        window.blit(self.texture,ncoord)
--- game.py
+++ game.py
@@ -6,64 +6,39 @@ from camera import Camera
 from entity import Entity
 
 def game_loop(window):
-    quitting=False
     clock = pygame.time.Clock()
-
-    blocks = [ Block(Config.BLOCK_SIZE*i,Config.WINDOW_H-Config.BLOCK_SIZE,Config.BLOCK_SIZE,Config.BLOCK_SIZE,True,f'Assets/Tileset/tileMain{2}.png') for i in range(1,20) ]
-    
-    player = Entity(10*Config.BLOCK_SIZE,Config.WINDOW_H-2*Config.BLOCK_SIZE,Config.BLOCK_SIZE,Config.BLOCK_SIZE,"Assets/GreenSlime/Grn_Idle1.png")
+    quitting=False
+    player = Entity.implement_player()
     camera = Camera(player)
-
-    back = pygame.transform.scale(pygame.image.load("Assets/Background/BG-sky.png"), (1920, 1080))
+  
+    blocks = Block.implement_blocks()
+    
+    back = Config.back
 
     while not quitting:
-        dt = 1/clock.get_fps() if clock.get_fps() != 0 else 1/Config.FPS
-        
+
         camera.update()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 quitting=True
 
-        keys_pressed = pygame.key.get_pressed()
-
-        player.acceleration.x = - player.vitesse.x*1000/Config.BLOCK_SIZE
-        player.acceleration.y = - player.vitesse.y*1000/Config.BLOCK_SIZE
-
-        if keys_pressed[pygame.K_d]:
-            player.acceleration.x += 2*Config.BLOCK_SIZE/dt
-
-        if keys_pressed[pygame.K_q]:
-            player.acceleration.x -= 2*Config.BLOCK_SIZE/dt
-
-        if keys_pressed[pygame.K_z]:
-            player.acceleration.y -= 2*Config.BLOCK_SIZE/dt
-
-        if keys_pressed[pygame.K_s]:
-            player.acceleration.y += 2*Config.BLOCK_SIZE/dt
-
-        player.vitesse.y += player.acceleration.y*dt
-        player.position.y += player.vitesse.y*dt
-
-        player.vitesse.x += player.acceleration.x*dt
-        player.position.x += player.vitesse.x*dt
+        player.move()
 
         window.blit(back,(0,0))
 
-        for block in blocks:
-            ncoord = ( block.position.x-camera.position.x+Config.WINDOW_W/2 , block.position.y-camera.position.y+Config.WINDOW_H/2 )
-            window.blit(block.texture,ncoord)
+        Block.blit_blocks(blocks, window, camera.position.x, camera.position.y)
         
-        ncoord = ( player.position.x-camera.position.x+Config.WINDOW_W/2-player.taille.x/2 , player.position.y-camera.position.y+Config.WINDOW_H/2)
-        window.blit(player.texture,ncoord)
+        player.blit_player(window, camera.position.x, camera.position.y)
 
         pygame.display.flip()
+        
         clock.tick_busy_loop(Config.FPS)
 
 
 if __name__ == '__main__':
     pygame.init()
-    window = pygame.display.set_mode((Config.WINDOW_W,Config.WINDOW_H),pygame.FULLSCREEN)
+    window = pygame.display.set_mode((Config.WINDOW_W,Config.WINDOW_H))
     pygame.display.set_caption("Premier Jeu")
     game_loop(window)
     pygame.quit()
