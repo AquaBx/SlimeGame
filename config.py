6f63a1473618f5434164c9c287acbe272176427a
vianney-herve <105049233+vianney-herve@users.noreply.github.com>
2022-12-07 15:39:44 +0100

    refactorisation des variables globales

--- config.py
+++ config.py
@@ -1,10 +1,19 @@
-import pygame
+import pygame as pg
+from pygame import Vector2 as v2, Surface
+from pygame.font import Font
 
 class GameConfig:
-    WINDOW_SIZE = pygame.Vector2(1280,720)
-    WINDOW = pygame.display.set_mode((WINDOW_SIZE.x, WINDOW_SIZE.y))
-    BLOCK_SIZE = WINDOW_SIZE.y / 12
-    FPS = 144
+    WINDOW_SIZE: v2 = v2(1280,720)
+    BLOCK_SIZE: float = WINDOW_SIZE.y / 12
+    FPS: float = 144.0
+
+    WINDOW: Surface
+    FONT: Font
+
+    def initialise() -> None:
+        pg.display.set_caption("Premier Jeu")
+        GameConfig.WINDOW = pg.display.set_mode((GameConfig.WINDOW_SIZE.x, GameConfig.WINDOW_SIZE.y))
+        GameConfig.FONT = Font(None, 30)
 
 class GameState:
-    dt = 1/60
+    dt: float = 1/60
