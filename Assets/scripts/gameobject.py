0a9a3744ad8ce895a53851180dd5ae7174d0a51a
vianney-herve <105049233+vianney-herve@users.noreply.github.com>
2022-12-13 10:04:59 +0100

    chargement de la map et gameobject

--- Assets/scripts/gameobject.py
+++ Assets/scripts/gameobject.py
@@ -18,15 +18,15 @@ class IGameObject(ABC):
 
 class GameObject(IGameObject):
 
-    def __init__(self, state: int, position: v2, taille: v2) -> None:
+    def __init__(self, state: int, position: v2) -> None:
         self.state: int = state
         self.position: v2 = position
-        self.taille: v2 = taille
+        self.taille: v2 = v2(GameConfig.BLOCK_SIZE, GameConfig.BLOCK_SIZE)
         self._mask = mask.Mask(self.taille, True)
 
+    def draw(self, camera: Camera) -> None: pass
     def update(self) -> None: pass
-    def draw(self) -> None: pass
-    
+
     @property
     def mask(self) -> Mask:
         return self._mask;
@@ -42,9 +42,9 @@ class GameObject(IGameObject):
 
 class Static(GameObject):
 
-    def __init__(self, state: int, position: v2, taille: v2, texture: Surface) -> None:
-        super().__init__(state, position, taille)
-        self.texture: Surface = texture
+    def __init__(self, state: int, position: v2, texture: Surface) -> None:
+        super().__init__(state, position)
+        self.texture: Surface = transform.scale(texture, self.taille)
         self.mask: Mask = mask.from_surface(texture)
 
     def draw(self, camera: Camera) -> None:
@@ -53,8 +53,8 @@ class Static(GameObject):
 
 class Dynamic(GameObject):
 
-    def __init__(self, state: int, position: v2, taille: v2, animations: list[Surface]) -> None:
-        super().__init__(state, position, taille)
+    def __init__(self, state: int, position: v2, animations: list[Surface]) -> None:
+        super().__init__(state, position)
 
         self.animations: list[Surface] = animations
         self.animation_frame: int = 0
@@ -76,23 +76,16 @@ class Dynamic(GameObject):
 
 class Ground(Static):
 
-    def __init__(self, index: int, position: v2, src: str) -> None:
-        texture = transform.scale(image.load(src), (GameConfig.BLOCK_SIZE, GameConfig.BLOCK_SIZE))
-        taille = v2(GameConfig.BLOCK_SIZE,GameConfig.BLOCK_SIZE)
-
-        super().__init__(index, position, taille, texture)
+    def __init__(self, index: int, position: v2, texture: Surface) -> None:
+        super().__init__(index, position, texture)
 
     def update(self) -> None: pass
 
 class Player(Dynamic):
 
-    def __init__(self, x: float, y: float, w: int, h: int, spritesheet: list[str]) -> None:
-        state: int = 0
-        position = v2(x, y)
-        taille = v2(w, h)
-        animations = [ transform.scale(image.load(src), (w, h)) for src in spritesheet ]
-
-        super().__init__(state, position, taille, animations)
+    def __init__(self, state: int, position: v2, spritesheet: list[str]) -> None:
+        animations = [ transform.scale(image.load(src), v2(GameConfig.BLOCK_SIZE, GameConfig.BLOCK_SIZE)) for src in spritesheet ]
+        super().__init__(state, position, animations)
 
     def update_frame(self) -> None:
         self.animation_frame += 13*GameState.dt
