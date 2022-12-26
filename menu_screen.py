d2daea887335e619524a7309d1f37b4228c4eff8
AquaBx <tom.chauvel@hotmail.com>
2022-12-26 22:16:21 +0100

    cleanup

--- menu_screen.py
+++ menu_screen.py
@@ -1,56 +1,52 @@
 import pygame
 from morgann_textes import Messages
-from config import GameConfig
+from config import Config
 
 class Menu :
-    # Cette classe permet d'afficher deux menus : le menu principal de début de jeu et le menu en jeu
+    # Cette classe permet d'afficher deux menus : le menu principal hors-partie et le menu en jeu
 
-    # Menu principal contient :
-    # Image de fond
-    # n rectangles (n est variable, pas de hardcode !!!)
-    # Dans les rectangles : "Nouvelle partie", "Quitter" ... 
-
-    # pygame.draw.rect(window, pygame.Color(255, 0, 0), (230,250,100,50))
+    # Le menu principal contient :
+        # Une image de fond
+        # Un rectangle par option de menu
+        # Dans chaque rectangle, on affiche une option
 
     def __init__(self) :
-        print("init menu")
-        self.adresse_fonte_blobbychug = "BLOBBYCHUG.ttf"
-        pygame.font.init()
+        self.adresse_fonte_blobbychug = "BLOBBYCHUG.ttf"    # On donne l'adresse de la fonte de texte utilisée
+        pygame.font.init()                                  # On initialise la fonte
 
-    def display_main_menu(window,options,background) :
-        # options est le tableau contenant les string des options affichées dans les rectangles : options = ["Nouvelle partie (N)", "Charger une partie (C)", "Quitter (Q)"] (changeable)
-
-        # Objectif de la fonction : afficher des rectangles avec les options, on met une image de fond qui recouvre la fenêtre
+    def display_main_menu(window,options,background) :  # Fonction permettant d'afficher le menu principal
+        # options est le tableau contenant les string des options affichées dans les rectangles : options = ["Continuer (C)", "Nouvelle partie (N)", "Charger une partie (H)", "Sauvegarder (S)", "Quitter (Q)"]
         
         messages = Messages()
-        n_rect = len(options)
-        intervalle = int(GameConfig.WINDOW_SIZE.y/(2*n_rect))
-        longueur_x = int(0.35*GameConfig.WINDOW_SIZE.x)
-        cote_gauche = int(0.325*GameConfig.WINDOW_SIZE.x)
+        n_rect = len(options)                           # Nombre de rectangles dans lesquels on affiche les options du menu
+        intervalle = int(Config.WINDOW_H/(2*n_rect))    # Intervalle vertical de pixels entre les rectangles
+        longueur_x = int(0.35*Config.WINDOW_W)          # Longueur horizontale de chaque rectangle
+        cote_gauche = int(0.325*Config.WINDOW_W)        # Côté gauche de chaque rectangle
 
-        # background_scaled = pygame.transform.scale(pygame.image.load(background), (GameConfig.WINDOW_SIZE.x, GameConfig.WINDOW_SIZE.y))
-        # window.blit(background_scaled,(0,0))
+        background_scaled = pygame.transform.scale(pygame.image.load(background), (Config.WINDOW_W, Config.WINDOW_H))   # On met l'image de fond à l'échelle de la fenêtre de jeu 
+        window.blit(background_scaled,(0,0))                                                                            # On affiche l'image de fond sur la fenêtre
+
         for i in range (0,n_rect) :
-            cote_haut = 10+int(intervalle/2)+i*intervalle*2
-            pygame.draw.rect(window, pygame.Color('brown'), (cote_gauche, cote_haut, longueur_x, intervalle))
-            messages.display_message(window, options[i], int(GameConfig.WINDOW_SIZE.x/2), cote_haut+int(intervalle/2), 30, pygame.Color('black'), False)
+            cote_haut = 10+int(intervalle/2)+i*intervalle*2                                                                                         # On détermine le côté haut des rectagnles un par un
+            pygame.draw.rect(window, pygame.Color('brown'), (cote_gauche, cote_haut, longueur_x, intervalle))                                       # On dessine les rectangles
+            messages.display_message(window, options[i], int(Config.WINDOW_W/2), cote_haut+int(intervalle/2), 30, pygame.Color('black'), False)     # On affiche le texte de l'option
 
-    def display_ingame_menu(window,options,background) :
+
+    def display_ingame_menu(window,options,background) :    # Fonction permettant d'afficher le menu en jeu
         
         messages = Messages()
         n_rect = len(options)                               # Nombre de rectangles dans lesquels on affiche les options du menu
-        intervalle = int((GameConfig.WINDOW_SIZE.y-20)/(2*n_rect))   # Intervalle vertical de pixels entre les rectangles
-        #longueur_y = intervalle
-        longueur_x = int(0.35*GameConfig.WINDOW_SIZE.x)              # Longueur horizontale de chaque rectangle
-        cote_gauche = int(0.325*GameConfig.WINDOW_SIZE.x)            # Côté gauche de chaque rectangle
+        intervalle = int((Config.WINDOW_H-20)/(2*n_rect))   # Intervalle vertical de pixels entre les rectangles
+        longueur_x = int(0.35*Config.WINDOW_W)              # Longueur horizontale de chaque rectangle
+        cote_gauche = int(0.325*Config.WINDOW_W)            # Côté gauche de chaque rectangle
 
         # Pour le tracé de l'image de fond, 2 choix : rectangle uni ou image
-        #pygame.draw.rect(window, pygame.Color('red'), (int(0.3*GameConfig.WINDOW_SIZE.x),10,int(0.4*GameConfig.WINDOW_SIZE.x),GameConfig.WINDOW_SIZE.y-20))
+        #pygame.draw.rect(window, pygame.Color('red'), (int(0.3*Config.WINDOW_W),10,int(0.4*Config.WINDOW_W),Config.WINDOW_H-20))   # Affichage du fond uni
 
-        background_scaled = pygame.transform.scale(pygame.image.load(background), (int(0.4*GameConfig.WINDOW_SIZE.x),GameConfig.WINDOW_SIZE.y-20))
-        window.blit(background_scaled,(int(0.3*GameConfig.WINDOW_SIZE.x),10))
+        background_scaled = pygame.transform.scale(pygame.image.load(background), (int(0.4*Config.WINDOW_W),Config.WINDOW_H-20))    # Mise à l'échelle d'une image de fond (menu central ne recouvrant pas toute la fenêtre)
+        window.blit(background_scaled,(int(0.3*Config.WINDOW_W),10))                                                                # Affichage du menu central
 
         for i in range (0,n_rect) :   # Affichage des textes des options
-            cote_haut = 10+int(intervalle/2)+i*intervalle*2
-            pygame.draw.rect(window, pygame.Color('blue'), (cote_gauche, cote_haut, longueur_x, intervalle))
-            messages.display_message(window, options[i], int(GameConfig.WINDOW_SIZE.x/2), cote_haut+int(intervalle/2), 30, pygame.Color("black"), False)
+            cote_haut = 10+int(intervalle/2)+i*intervalle*2                                                                                     # On détermine le côté haut des rectagnles un par un
+            pygame.draw.rect(window, pygame.Color('blue'), (cote_gauche, cote_haut, longueur_x, intervalle))                                    # On dessine les rectangles
+            messages.display_message(window, options[i], int(Config.WINDOW_W/2), cote_haut+int(intervalle/2), 30, pygame.Color("black"), False) # On affiche le texte de l'option
--- morgann_textes.py
+++ morgann_textes.py
@@ -3,54 +3,54 @@ import pygame
 class Messages :
 
     def __init__(self):
-        self.adresse_fonte_bradbunr = "BradBunR.ttf" # On pourra modifier l'adresse à laquelle se trouve la fonte plus facilement car elle est dans une variable
-        self.adresse_fonte_blobbychug = "BLOBBYCHUG.ttf" # Il y a autant de variables d'adresses de fontes que de fontes différentes
-        self.adresse_fonte_pressstart2p = "PressStart2P-Regular.ttf" # Cette police a une taille beaucoup moins régulière, ce qui ne m'arrange pas vraiment
-        self.used_font = self.adresse_fonte_blobbychug # à changer à volonté !
-        pygame.font.init()
-        self.GREY = (51,51,51)
+        self.adresse_fonte_bradbunr = "BradBunR.ttf"                    # On pourra modifier l'adresse à laquelle se trouve la fonte plus facilement car elle est dans une variable
+        self.adresse_fonte_blobbychug = "BLOBBYCHUG.ttf"                # Il y a autant de variables d'adresses de fontes que de fontes différentes
+        self.adresse_fonte_pressstart2p = "PressStart2P-Regular.ttf"    # Cette police a une taille beaucoup moins régulière, ce qui ne m'arrange pas vraiment
+        self.used_font = self.adresse_fonte_blobbychug                  # On choisit l'une des fontes de texte
+        pygame.font.init()                                              # Initialisation de la fonte
+        self.GREY = (51,51,51)                                          # Initialisations de plusieurs couleurs
         self.RED = (255,0,0)
         self.BLUE = (0,0,255)
         self.GREEN = (0,255,0)
 
-        self.FONT_TAB = 151 * [0] # construction du tableau contenant toutes les tailles de fonte possibles dans un intervalle donné
+        self.FONT_TAB = 151 * [0] # Construction du tableau contenant toutes les tailles de fonte possibles dans un intervalle donné
         for i in range (0,151) :
             self.FONT_TAB[i] = pygame.font.Font(self.used_font,i)
     
-    def is_a_letter(letter) :
+    def is_a_letter(letter) : # Fonction permettant de déterminer si une chaîne est une lettre (Pour savoir à quel moment mettre un tiret à la coupure d'une ligne)
         if letter in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" :
             return True
         else :
             return False
 
-    def dividing_the_string(self,text,font_size) : # Peut-être que faire ça en itératif est mieux qu'en récursif ?
-        k = 30 # Ce k va devoir dépendre de font_size, il paraît ajusté pour valeur 30
-        t = []
+    def dividing_the_string(self,text,font_size) :      # Fonction permettant de diviser une chaîne de caractères en plus petites chaînes 
+        k = 30                                          # Nombre de caractères par ligne
+        t = []                                          # Initialisation du tableau dont chaque case contiendra une ligne à afficher 
         while True :
-            if font_size*len(text) > 20 : # Il faudra affiner ce seuil par l'expérimentation
-                new_line = text[0:k]
-                if k+1 < len(text) :
+            if font_size*len(text) > 20 :               # Seuillage pour couper en lignes
+                new_line = text[0:k]                    # Création d'une nouvelle ligne
+                if k+1 < len(text) :                    # On répète l'opération jusqu'à arriver au bout du message
                     if Messages.is_a_letter(text[k-1]) and Messages.is_a_letter(text[k]) : # Si les caractères autour du point de découpage sont des lettres alors on ajoute un tiret à la fin de la ligne car on a coupé un mot
                         new_line += "-"
-                t.append(new_line)
-                text = text[k:len(text)]
+                t.append(new_line)                      # Ajout de la nouvelle ligne dans le tableau
+                text = text[k:len(text)]                # On retire le texte de la ligne d'origine pour en traiter la suite
             else :
-                t.append(text)
-                break
+                t.append(text)                          # Si le texte restant est trop court on l'ajoute dans le tableau sans le modifier
+                break                                   # On sort ensuite de la boucle
         return t
     
     def display_message(self,window,text,x,y,font_size,color,divide) :
-        # Le paramètre divide est un booléen : si True alors on affichera le texte sur plusieurs lignes, sinon sur une seule ligne
+        # Le paramètre divide est un booléen : si True alors on affichera le texte sur plusieurs lignes grâce à la fonction dividing_the_string, sinon sur une seule ligne
         if divide == True :
-            text_tab = Messages.dividing_the_string(self,text,font_size) # Si la chaîne est trop longue on la découpe en plusieurs lignes
-            for i in range (0,len(text_tab)-1) : # On répète l'opération d'affichage de texte autant de fois qu'il y a de lignes
+            text_tab = Messages.dividing_the_string(self,text,font_size)        # Si la chaîne est trop longue on la découpe en plusieurs lignes
+            for i in range (0,len(text_tab)-1) :                                # On répète l'opération d'affichage de texte autant de fois qu'il y a de lignes
                 img = self.FONT_TAB[font_size-1].render(text_tab[i],True,color) # On crée l'image en sélectionnant la fonte associée dans le tableau, puis le texte
-                display_rect = img.get_rect() # On crée le rectangle dans lequel sera affichée l'image
-                display_rect.center = (x, y+i*font_size) # On centre le rectangle sur les coordonnées entrées en paramètres
-                window.blit(img,display_rect) # On affiche l'image dans son rectangle
+                display_rect = img.get_rect()                                   # On crée le rectangle dans lequel sera affichée l'image
+                display_rect.center = (x, y+i*font_size)                        # On centre le rectangle sur les coordonnées entrées en paramètres
+                window.blit(img,display_rect)                                   # On affiche l'image dans son rectangle
         
         else :
-            img = self.FONT_TAB[font_size-1].render(text,True,color) # On crée l'image en sélectionnant la fonte associée dans le tableau, puis le texte
-            display_rect = img.get_rect() # On crée le rectangle dans lequel sera affichée l'image
-            display_rect.center = (x, y) # On centre le rectangle sur les coordonnées entrées en paramètres
-            window.blit(img,display_rect) # On affiche l'image dans son rectangle
+            img = self.FONT_TAB[font_size-1].render(text,True,color)    # On crée l'image en sélectionnant la fonte associée dans le tableau, puis le texte
+            display_rect = img.get_rect()                               # On crée le rectangle dans lequel sera affichée l'image
+            display_rect.center = (x, y)                                # On centre le rectangle sur les coordonnées entrées en paramètres
+            window.blit(img,display_rect)                               # On affiche l'image dans son rectangle
