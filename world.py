f35e939f31200f0f9d82f945ee224e8160c62c19
AquaBx <tom.chauvel@hotmail.com>
2022-12-26 18:52:00 +0100

    update

--- world.py
+++ world.py
@@ -56,9 +56,8 @@ class World():
             for j in range(grid_columns):
                 (id, state, uuid) = struct.unpack("@bbh", f.read(4))
 
-                shine = True if id == 5 else False
+                shine = True if id == 4 else False
  
-                print(id)
                 if id == -1: continue
                 self.blocks[i, j] = table[id].script.create((i, j), id, state, uuid,shine)
                 # StateElement(id, state, uuid, v2(j*Grid.tile_size, i*Grid.tile_size), palette.elements[id].spritesheet[state])
