4db4b42af20523e1a1e05152ecbcab3d8f73bc1c
AquaBx <tom.chauvel@hotmail.com>
2022-12-27 00:22:40 +0100

    changes v1

--- config.py
+++ config.py
@@ -10,16 +10,30 @@ class GameConfig:
     WINDOW: Surface
     GAME_SURFACE: Surface
 
-    FONT: Font
-    back = pg.transform.scale(pg.image.load("main_background.jpg"), WINDOW_SIZE.xy)
 
     opacity_world = 150
 
+    FONTS = {
+        "BradBunR":"BradBunR.ttf",
+        "BLOBBYCHUG":"BLOBBYCHUG.ttf",
+        "PressStart2P":"PressStart2P-Regular.ttf"
+    }
+
+    COLORS  = {
+        "GREY": (51,51,51),
+        "RED" : (255,0,0),
+        "BLUE" : (0,0,255),
+        "GREEN" : (0,255,0)
+    }
+
     def initialise() -> None:
-        pg.display.set_caption("Premier Jeu")
+        pg.display.set_caption("Slime Game")
+        pg.font.init()
         GameConfig.WINDOW = pg.display.set_mode(GameConfig.WINDOW_SIZE.xy)
         GameConfig.GAME_SURFACE = pg.Surface((GameConfig.WINDOW_SIZE.x*9*GameConfig.BLOCK_SIZE/GameConfig.WINDOW_SIZE.y,9*GameConfig.BLOCK_SIZE))
-        GameConfig.FONT = Font(None, 30)
+
+    def FONT(font,size): 
+        return Font(GameConfig.FONTS[font], size)
 
 class GameState:
     dt: float = 1/60
