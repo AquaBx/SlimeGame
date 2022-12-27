512a4268ef0a344e571e4e0796d498ea0d2a36d7
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2022-12-27 21:25:08 +0100

    ajout intensity dans les shader

--- shader.py
+++ shader.py
@@ -5,7 +5,8 @@ from config import GameConfig, GameState
 def pilImageToSurface(pilImage):
     return pygame.image.fromstring(pilImage.tobytes(), pilImage.size, pilImage.mode)
 
-def draw_a_light(center,color,radius):
+def draw_a_light(center,color,radius,intensity=75):
+
     color_layer = Image.new('RGBA', GameConfig.GAME_SURFACE.get_size() , color)
     mask = Image.new("L", color_layer.size, (0))
     draw = ImageDraw.Draw(mask)
@@ -14,14 +15,17 @@ def draw_a_light(center,color,radius):
     p2 = (center[0]+radius,center[1]+radius)
     shape = (p1,p2)
 
-    draw.ellipse(shape, fill=75)
+    draw.ellipse(shape, fill=intensity)
 
     mask_blur = mask.filter(ImageFilter.GaussianBlur(radius/2))
-    GameState.shader = Image.composite(color_layer, GameState.shader, mask_blur)
 
+    GameState.shader = Image.composite(color_layer, GameState.shader, mask_blur)
+    
 def reset():
     GameState.shader = Image.new('RGBA', GameConfig.GAME_SURFACE.get_size() , (0,0,15,GameConfig.opacity_world))
 
 def draw():
+    
     img = pilImageToSurface(GameState.shader)
     GameConfig.GAME_SURFACE.blit(img,(0,0))
+    
