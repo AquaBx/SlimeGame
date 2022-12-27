1dfbdef82d45dabf68beaedc244eb71a9b8dc5e4
Josso Celia <celia.josso@etudiant.univ-rennes1.fr>
2022-12-27 23:31:52 +0000

    Ajout de la fonctionnalité permettant d'afficher du texte en jeu selon les...

--- assets/scripts/gameobject.py
+++ assets/scripts/gameobject.py
@@ -1,14 +1,14 @@
 from abc import ABC, abstractclassmethod
-from xmlrpc.client import Boolean, boolean
 
 import pygame as pg
-from pygame import key, image, mask, transform,Color
+from pygame import key, image, mask, transform
 from pygame import Vector2 as v2, Rect, Surface
 from pygame.mask import Mask
 
 from camera import Camera
 from config import GameConfig, GameState
 from input import Input
+
 from assets.palette import Palette
 
 import shader
@@ -92,11 +92,12 @@ class Static(GameObject):
 
 class Dynamic(GameObject):
 
-    def __init__(self, state: int, position: v2, taille:v2, animations: list[Surface]) -> None:
+    def __init__(self, state: int, position: v2, taille:v2, animations: dict[str, list[Surface]]) -> None:
         GameObject.__init__(self, state, position,taille)
 
-        self.animations: list[Surface] = animations
+        self.animations: dict[str, list[Surface]] = animations
         self.animation_frame: int = 0
+        self.current_animation: str = "idle"
         self.sante : int = 20
         self.santemax : int = 20
         self.is_flying : bool = True
@@ -110,7 +111,7 @@ class Dynamic(GameObject):
 
     @property
     def texture(self) -> Surface:
-        return self.animations[self.animation_frame]
+        return self.animations[self.current_animation][self.animation_frame]
 
 EmptyElement: GameObject = GameObject(0, v2(0, 0), v2(0, 0))
 
@@ -149,18 +150,86 @@ class Lamp(Static, LigthSource):
         GameConfig.GAME_SURFACE.blit(self.texture, rect)
 
 class Player(Dynamic, LigthSource):
+    
+    __animation_sprites: list[list[str]] = [
+        [f"assets/sprites/dynamics/GreenSlime/Grn_Idle{i}.png" for i in range(1,11)],
+        [f"assets/sprites/dynamics/GreenSlime/Grn_HighJump{i}.png" for i in range(1,11)],
+        [f"assets/sprites/dynamics/GreenSlime/Grn_Death{i}.png" for i in range(1,6)],
+        [f"assets/sprites/dynamics/GreenSlime/Grn_Right{i}.png" for i in range(1,6)],
+        [f"assets/sprites/dynamics/GreenSlime/Grn_Left{i}.png" for i in range(1,6)],
+        [f"assets/sprites/dynamics/GreenSlime/Grn_Dmg{i}.png" for i in range(1,6)]
+    ]
 
-    def __init__(self, position: v2, taille: v2, spritesheet: list[str]) -> None:
+    def __init__(self, position: v2, taille: v2) -> None:
         state: int = 0
-        animations = [ transform.scale(image.load(src), (taille.x, taille.y)) for src in spritesheet ]
+        self.status_frame: float = 0
+        animations: dict[str, list[Surface]] = {
+            "idle": [ transform.scale(image.load(src), (taille.x, taille.y)) for src in Player.__animation_sprites[0]],
+            "jump": [ transform.scale(image.load(src), (taille.x, 3*taille.y)) for src in Player.__animation_sprites[1]],
+            "death": [ transform.scale(image.load(src), (taille.x, taille.y)) for src in Player.__animation_sprites[2]],
+            "right": [ transform.scale(image.load(src), (taille.x, taille.y)) for src in Player.__animation_sprites[3]],
+            "left": [ transform.scale(image.load(src), (taille.x, taille.y)) for src in Player.__animation_sprites[4]],
+            "damage": [ transform.scale(image.load(src), (taille.x, taille.y)) for src in Player.__animation_sprites[5]]
+            # "attack": [ transform.scale(image.load(src), (taille.x, taille.y)) for src in Player.__animation_sprites[6]]
+            # "fall": [ transform.scale(image.load(src), (taille.x, taille.y)) for src in Player.__animation_sprites[7]]
+            # "interaction": [ transform.scale(image.load(src), (taille.x, taille.y)) for src in Player.__animation_sprites[8]]
+        }
+        super().__init__(state, position, taille, animations)
         Dynamic.__init__(self, state, position, taille, animations)
         LigthSource.__init__(self)
         self.glow = (178, 230, 119, 255)
-        self._mask = mask.from_surface(animations[0])
+        self._mask = mask.from_surface(animations["idle"][0])
 
-    def update_frame(self) -> None:
-        self.animation_frame += 13*GameState.dt
-        self.animation_frame = int(self.animation_frame % len(self.animations))
+    def update_frame(self, is_flying) -> None:
+        self.status_frame = (self.status_frame+15*GameState.dt) % 10
+        # jump_counter = 1
+
+        # condition pour self.current_animation = "jump"
+        # condition pour self.current_animation = "fall"
+
+        # touches provisoires pour tester les frames
+        if Input.is_pressed(pg.K_p):
+            self.current_animation = "death"
+        elif Input.is_pressed(pg.K_o):
+            self.current_animation = "damage"
+        # elif pressed[pg.K_i]:
+        #     self.current_animation = "attack"
+        # elif pressed[pg.K_u]:
+        #     self.current_animation = "interaction"
+
+        # propre
+        elif Input.is_pressed(pg.K_d):
+            self.current_animation = "right"
+        elif Input.is_pressed(pg.K_q):
+            self.current_animation = "left"
+        else:
+            self.current_animation = "idle"
+
+        pos_avant = v2(self.position.x,self.position.y)
+
+        dir_y = self.position.y - pos_avant[1]
+
+        # if self.current_animation == "jump":
+        #     if is_flying: # si le slime vole -> frames 1, 2, 3, 4, 5, 6, 7
+        #         if dir_y > 0 : # si le slime descend -> frames 6 7
+        #             if jump_counter >= 7:
+        #                 self.status_frame = 7
+        #             elif 6 <= jump_counter <= 7:
+        #                 self.status_frame = jump_counter
+        #         elif dir_y < 0: # si le slime monte -> frames 1 2 3 4
+        #             if 1 < jump_counter < 5:
+        #                 self.status_frame = jump_counter
+        #         else : # si le slime reste sur place -> frame 5
+        #             if jump_counter == 5:
+        #                 self.status_frame = 5
+        #     else : # si le slime ne vole pas -> frames 0 8 9
+        #         if jump_counter == 8 or jump_counter == 9:
+        #             self.status_frame = jump_counter # je test
+        #     jump_counter += 1
+
+        self.status_frame -= GameState.dt
+        self.animation_frame = int(self.status_frame)
+        self.animation_frame = int(self.animation_frame % len(self.animations[self.current_animation]))
 
     def update(self) -> None:
         if Input.is_pressed(pg.K_d):
