78a8b98d95be91737594ebf8fb286415be86b406
Vianney HERVE <vianney.herve@etudiant.univ-rennes1.fr>
2022-12-27 02:16:13 +0100

    moved fonts and fixed size of font

--- config.py
+++ config.py
@@ -13,17 +13,20 @@ class GameConfig:
 
     opacity_world = 150
 
-    FONTS = {
-        "BradBunR":"BradBunR.ttf",
-        "BLOBBYCHUG":"BLOBBYCHUG.ttf",
-        "PressStart2P":"PressStart2P-Regular.ttf"
+    FONT_SIZE: int = 25
+    FONT_DIR: str = "assets/fonts"
+    FONTS: dict[str, Font] = {}
+    __FONTS_DATA: dict[str, str] = {
+        "BradBunR": f"{FONT_DIR}/BradBunR.ttf",
+        "BLOBBYCHUG": f"{FONT_DIR}/BLOBBYCHUG.ttf",
+        "PressStart2P": f"{FONT_DIR}/PressStart2P-Regular.ttf"
     }
 
-    COLORS  = {
-        "GREY": (51,51,51),
-        "RED" : (255,0,0),
-        "BLUE" : (0,0,255),
-        "GREEN" : (0,255,0)
+    COLORS: dict[str, tuple[int, int, int]] = {
+        "GREY":  (51, 51, 51),
+        "RED":   (255, 0, 0),
+        "BLUE":  (0, 0, 255),
+        "GREEN": (0, 255, 0)
     }
 
     def initialise() -> None:
@@ -31,9 +34,8 @@ class GameConfig:
         pg.font.init()
         GameConfig.WINDOW = pg.display.set_mode(GameConfig.WINDOW_SIZE.xy)
         GameConfig.GAME_SURFACE = pg.Surface((GameConfig.WINDOW_SIZE.x*9*GameConfig.BLOCK_SIZE/GameConfig.WINDOW_SIZE.y,9*GameConfig.BLOCK_SIZE))
-
-    def FONT(font,size): 
-        return Font(GameConfig.FONTS[font], size)
+        for name, path in GameConfig.__FONTS_DATA.items():
+            GameConfig.FONTS[name] = Font(path, GameConfig.FONT_SIZE)
 
 class GameState:
     dt: float = 1/60
