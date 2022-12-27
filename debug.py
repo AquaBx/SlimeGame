9f9b2283d5846db8bff6a8f342fee3a15e10f67b
tchauvel <tom.chauvel@etudiant.univ-rennes1.fr>
2022-12-27 22:09:33 +0100

    test

--- debug.py
+++ debug.py
@@ -2,7 +2,7 @@ from pygame import draw
 from config import GameConfig
 
 def debug(info: object, y: float = 10, x: float = 10) -> None:
-    debug_surf = GameConfig.FONT.render(str(info), True, 'White')
+    debug_surf = GameConfig.FONTS["PressStart2P"].render(str(info), True, 'White')
     debug_rect = debug_surf.get_rect(topleft = (x, y))
     draw.rect(GameConfig.WINDOW, 'Black', debug_rect)
 
--- shader.py
+++ shader.py
@@ -14,18 +14,14 @@ def draw_a_light(center,color,radius,intensity=75):
     p1 = (center[0]-radius,center[1]-radius)
     p2 = (center[0]+radius,center[1]+radius)
     shape = (p1,p2)
-
+    
     draw.ellipse(shape, fill=intensity)
-
-    mask_blur = mask.filter(ImageFilter.GaussianBlur(radius/2))
-
-    GameState.shader = Image.composite(color_layer, GameState.shader, mask_blur)
+    GameState.shader = Image.composite(color_layer, GameState.shader, mask)
     
 def reset():
     GameState.shader = Image.new('RGBA', GameConfig.GAME_SURFACE.get_size() , (0,0,15,GameConfig.opacity_world))
 
 def draw():
-    
-    img = pilImageToSurface(GameState.shader)
+    img = pilImageToSurface(GameState.shader.filter(ImageFilter.GaussianBlur(10)))
     GameConfig.GAME_SURFACE.blit(img,(0,0))
     
