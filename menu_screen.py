914dc6f5178ebb5104fbab01dfb4578e13ff9d05
Vianney HERVE <vianney.herve@etudiant.univ-rennes1.fr>
2022-12-27 01:10:01 +0100

    resolve

--- menu_screen.py
+++ menu_screen.py
@@ -1,4 +1,6 @@
 import pygame
+from pygame import Vector2 as v2
+
 from morgann_textes import Messages
 from config import GameConfig
 
@@ -32,7 +34,7 @@ class Menu :
         for i in range (0,n_rect) :
             cote_haut = 10+int(intervalle/2)+i*intervalle*2                                                                                                  # On détermine le côté haut des rectagnles un par un
             pygame.draw.rect(window, pygame.Color('brown'), (cote_gauche, cote_haut, longueur_x, intervalle))                                                # On dessine les rectangles
-            messages.display_message(window, options[i], int(GameConfig.WINDOW_SIZE.x/2), cote_haut+int(intervalle/2),"PressStart2P", 30, pygame.Color('black'), False)     # On affiche le texte de l'option
+            messages.display_message(options[i], v2(GameConfig.WINDOW_SIZE.x/2, cote_haut+(intervalle/2)),"PressStart2P", 30, pygame.Color('black'), False)     # On affiche le texte de l'option
 
 
     def display_ingame_menu(options) :
@@ -55,4 +57,4 @@ class Menu :
         for i in range (0,n_rect) :   # Affichage des textes des options
             cote_haut = 10+int(intervalle/2)+i*intervalle*2                                                                                              # On détermine le côté haut des rectagnles un par un
             pygame.draw.rect(window, pygame.Color('blue'), (cote_gauche, cote_haut, longueur_x, intervalle))                                             # On dessine les rectangles
-            messages.display_message(window, options[i], int(GameConfig.WINDOW_SIZE.x/2), cote_haut+int(intervalle/2), "PressStart2P", 30, pygame.Color("black"), False) # On affiche le texte de l'option
+            messages.display_message(options[i], v2(GameConfig.WINDOW_SIZE.x/2, cote_haut+(intervalle/2)), "PressStart2P", 30, pygame.Color("black"), False) # On affiche le texte de l'option
