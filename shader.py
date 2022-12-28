991a8b1b860ad5d8f1d306e0dc177db3936af90b
tchauvel <tom.chauvel@etudiant.univ-rennes1.fr>
2022-12-28 20:14:15 +0100

    light plus jolie

--- shader.py
+++ shader.py
@@ -1,31 +1,41 @@
-import pygame
+import pygame as pg
 from PIL import Image, ImageDraw,ImageFilter
 from config import GameConfig, GameState
 
 def pilImageToSurface(pilImage):
-    return pygame.image.fromstring(pilImage.tobytes(), pilImage.size, pilImage.mode).convert_alpha()
+    return pg.image.fromstring(pilImage.tobytes(), pilImage.size, pilImage.mode).convert_alpha()
+
+def SurfaceTopilImage(surface):
+    surf = pg.transform.rotate(surface,-90)
+    surf = pg.transform.flip(surf,True,False)
+    arr = pg.surfarray.array3d(surf)
+    return Image.fromarray(arr)
+
 
 def draw_a_light(center,color,radius,intensity=75):
     if not GameConfig.Graphics.EnableLights : return
-    color_layer = Image.new('RGBA', GameConfig.GAME_SURFACE.get_size() , color)
-    mask = Image.new("L", color_layer.size, (0))
+    mask = Image.new("RGBA", GameConfig.GAME_SURFACE.get_size() , (intensity))
     draw = ImageDraw.Draw(mask)
 
     p1 = (center[0]-radius,center[1]-radius)
     p2 = (center[0]+radius,center[1]+radius)
     shape = (p1,p2)
     
-    draw.ellipse(shape, fill=intensity)
-    GameState.shader = Image.composite(color_layer, GameState.shader, mask)
+    draw.ellipse(shape, fill=color)
+
+    circle = pilImageToSurface( mask )
+    GameState.shader.blit(circle,(0,0))
     
 def reset():
     if not GameConfig.Graphics.EnableLights : return
 
-    GameState.shader = Image.new('RGBA', GameConfig.GAME_SURFACE.get_size() , (0,0,15,GameConfig.opacity_world))
+    GameState.shader = pg.Surface(GameConfig.GAME_SURFACE.get_size())
+    GameState.shader.fill((15,15,15,GameConfig.opacity_world))
 
 def draw():
     if not GameConfig.Graphics.EnableLights : return
-
-    img = pilImageToSurface(GameState.shader.filter(ImageFilter.GaussianBlur(10)))
-    GameConfig.GAME_SURFACE.blit(img,(0,0))
+    img = SurfaceTopilImage(GameState.shader)
+    img = img.filter( ImageFilter.GaussianBlur(10) )
+    img = pilImageToSurface(img)
+    GameConfig.GAME_SURFACE.blit(img,(0,0),special_flags=pg.BLEND_MULT)
     
