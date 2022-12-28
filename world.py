d46ebf4f2096141d844d5d8bf0af114d1ee38c9a
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2022-12-29 00:56:48 +0100

    comment world draw

--- world.py
+++ world.py
@@ -72,27 +72,36 @@ class World:
 
     def draw(self) -> None:
 
+        """ initialisation du draw """
         surface_size = GameState.GAME_SURFACE.get_size()
-        shader.reset()
+        shader.reset() # initialisation des lights
+        GameState.GAME_SURFACE.fill('Black')
 
+        """ affichage du monde """
+        # itération de tout les blocks visibles
         for j in range( max(0, int(self.camera.rect.left / GameConfig.BLOCK_SIZE ) ) , min( len(self.blocks[0]) , int(self.camera.rect.right / GameConfig.BLOCK_SIZE ) + 1 ) ):
             for i in range( max(0, int(self.camera.rect.top / GameConfig.BLOCK_SIZE ) ) , min( len(self.blocks) ,  int(self.camera.rect.bottom / GameConfig.BLOCK_SIZE) + 1 ) ):
                 self.blocks[i, j].draw(self.camera)
 
-        link = self.camera.link
-        link.draw(self.camera)
+        """ affichage des joueurs / mobs """
+        self.player.draw(self.camera)
+
+        """ draw lights """
         shader.draw()
 
-        link.sante -= 0.0001
-        
-        scale = 1/7*surface_size[1]/20
+        """ affichage UI """
+        # recuperation de l'entité lié à la caméra
+        link = self.camera.link
+
+        # draw healthbar
         mr_bottom = surface_size[1]-2*surface_size[1]/20
-
+        scale = 1/7*surface_size[1]/20
         health_percent = link.sante/link.santemax * 29 * scale
-
         pg.draw.rect( GameState.GAME_SURFACE, (255,7,3) , pg.Rect(surface_size[1]/20+10*scale,mr_bottom+2*scale,health_percent,3*scale))
-
         GameState.GAME_SURFACE.blit( GameConfig.HealthBar,(surface_size[1]/20,mr_bottom) )
+        
+        """ blit fenetre """
+        # upscale sur la taille de la fenetre
         GameState.WINDOW.blit(transform.scale(GameState.GAME_SURFACE, GameState.WINDOW.get_size()),(0,0))
 
     def gravite(self,obj):
