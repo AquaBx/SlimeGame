774d41efbb578870f3facd61119aad8108fc1e70
Malo | Astri <74017420+Astri2@users.noreply.github.com>
2022-12-29 03:37:48 +0100

    implement button class

---
+++ button_script.py
@@ -0,0 +1,14 @@
+
+from typing import Any
+
+class ButtonScript:
+    def __init__(self, fct, *args) -> None:
+        self.fct = fct
+        self.args = args
+
+    def __call__(self, *args: Any, **kwds: Any) -> Any:
+        #eventuel override des arguments prédéfinis a cet endroit
+        self.fct(*self.args)
+
+    def print_mouse_pos(mouse):
+        print(f"mouse position : {mouse.get_pos()}")
