b6fb6b559fdc1fdfeb48a858f60cbe4c3c456209
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2022-12-29 08:00:58 +0100

    cleanup v2

--- world/collide.py
+++ world/collide.py
@@ -1,6 +1,6 @@
-from pygame import Vector2 as v2
+from pygame import Vector2 as v2,Rect
 from pygame.mask import Mask
-from assets.scripts.gameobject import GameObject, Dynamic, Player, EmptyElement
+from assets.scripts.gameobject import Dynamic, EmptyElement
 
 def collide(self,obj: Dynamic):
     # we get all 9 blocs based on the centered position of the player (0)
--- world/deserialize.py
+++ world/deserialize.py
@@ -10,29 +10,27 @@ from config import GameState
 
 def deserialize(self, file: str) -> None:
     # on ouvre le fichier dans le dossier .../slime_game/assets/maps/
-    f = open(f"assets/maps/{file}.map", "rb")
+    with open(f"assets/maps/{file}.map", "rb") as f:
+        # on récupère en premier les informations de la palette
 
-    # on récupère en premier les informations de la palette
+        # on lit la taille de la palette
+        table_length: int  = struct.unpack("@b", f.read(1))[0]
 
-    # on lit la taille de la palette
-    table_length: int  = struct.unpack("@b", f.read(1))[0]
+        # construit la liste des assets
+        table = [ ASSETS[global_id] for global_id in list(struct.unpack("@" + "h"*table_length, f.read(2*table_length)))]
+        Palette.load(table)
 
-    # construit la liste des assets
-    table = [ ASSETS[global_id] for global_id in list(struct.unpack("@" + "h"*table_length, f.read(2*table_length)))]
-    Palette.load(table)
+        # on lit ensuite les dimensions de la grille
+        (grid_rows, grid_columns) = struct.unpack("@bb", f.read(2))
+        self.blocks = np.full((grid_rows, grid_columns), EmptyElement)
+        for i in range(grid_rows):
+            for j in range(grid_columns):
+                (id, state, uuid) = struct.unpack("@bbh", f.read(4))
 
-    # on lit ensuite les dimensions de la grille
-    (grid_rows, grid_columns) = struct.unpack("@bb", f.read(2))
-    self.blocks = np.full((grid_rows, grid_columns), EmptyElement)
-    for i in range(grid_rows):
-        for j in range(grid_columns):
-            (id, state, uuid) = struct.unpack("@bbh", f.read(4))
+                if id == -1: continue
+                self.blocks[i, j] = table[id].script.create((i, j), id, state, uuid)
 
-            if id == -1: continue
-            self.blocks[i, j] = table[id].script.create((i, j), id, state, uuid)
+        # enfin on lit le background de la map
+        background_id: int = struct.unpack("@h", f.read(2))[0]
 
-    # enfin on lit le background de la map
-    background_id: int = struct.unpack("@h", f.read(2))[0]
-
-    self.background: Surface = transform.scale(image.load(ASSETS[background_id].path).convert(), GameState.GAME_SURFACE.get_size())
-    f.close()
+        self.background: Surface = transform.scale(image.load(ASSETS[background_id].path).convert(), GameState.GAME_SURFACE.get_size())
