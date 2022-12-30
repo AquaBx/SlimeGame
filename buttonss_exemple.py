8f928b12cfa896a4ec38848093781a13e6ef5512
Astri_ <malo1.thiebaud@gmail.com>
2022-12-30 02:26:09 +0100

    update the example file

--- buttonss_exemple.py
+++ buttonss_exemple.py
@@ -13,36 +13,89 @@ from input import Input
 from morgann_textes import Text
 from config import GameConfig
 
+from menu_screen import Menu
 
-pygame.init()
 
-window = display.set_mode((500,500))
-clock = Clock()
-texture = image.load(f"{ASSET_DIR}/UI/button1.png")
-should_quit = False
+class Game:
+    def __init__(self):
+        self.should_quit = False
 
-Input.init()
-ButtonManager.init(window)
-Text.init({"BradBunR": f"{GameConfig.FONT_DIR}/BradBunR.ttf"}, GameConfig.FONT_SIZE)
 
-b = Button("bouton1", "bonjour", Rect(10,10,100,100),ButtonScript(ButtonScript.print_mouse_pos, pygame.mouse),texture,alive=True,enabled=True,label_color=Color("red"))
+def button_example():
 
-while not should_quit:
-    #l'appel de get est important pour update notamment les inputs claviers 
-    for ev in pygame.event.get():
-        if ev.type == pygame.QUIT:
-            should_quit = True
+    pygame.init()
 
-    Input.update()
+    game = Game()
+    window = display.set_mode((500,500))
+    clock = Clock()
+    texture = image.load(f"{ASSET_DIR}/UI/button1.png")
+    should_quit = False
 
-    if Input.is_pressed_once(pygame.K_z):
-        if ButtonManager.is_alive(b.id):
-            ButtonManager.kill(b.id)
-        else:
-            ButtonManager.set_alive(b.id)
+    Input.init()
+    Text.init(window, {"BradBunR": f"{GameConfig.FONT_DIR}/BradBunR.ttf"}, GameConfig.FONT_SIZE)
+    ButtonManager.init(window)
 
-    window.fill("black")
-    ButtonManager.update()
-    display.update()
+    Bid = "bouton1"
+    Button(Bid, "bonjour", Rect(10,10,100,100),ButtonScript(ButtonScript.print_mouse_pos, pygame.mouse),texture,alive=True,enabled=True,label_color=Color("red"))
+    
+    while not game.should_quit:
+            #l'appel de get est important pour update notamment les inputs claviers 
+            for ev in pygame.event.get():
+                if ev.type == pygame.QUIT:
+                    game.should_quit = True
 
-    clock.tick_busy_loop(70)
+            Input.update()
+
+            if Input.is_pressed_once(pygame.K_z):
+                if ButtonManager.is_alive(Bid):
+                    ButtonManager.kill(Bid)
+                else:
+                    ButtonManager.set_alive(Bid)
+
+            window.fill("black")
+            ButtonManager.update()
+            display.update()
+
+            clock.tick_busy_loop(70)
+
+def menu_example():
+    """il faut modifier les valeurs de la taille du menu dans menu_screen : Menu#__create_ingame_menu
+    """
+
+    pygame.init()
+
+    game = Game()
+    window = display.set_mode((500,500))
+    clock = Clock()
+    texture = image.load(f"{ASSET_DIR}/UI/button1.png")
+    should_quit = False
+
+    Input.init()
+    Text.init(window, {"BradBunR": f"{GameConfig.FONT_DIR}/BradBunR.ttf"}, GameConfig.FONT_SIZE)
+    ButtonManager.init(window)
+    Menu.init(game)
+
+    while not game.should_quit:
+        #l'appel de get est important pour update notamment les inputs claviers 
+        for ev in pygame.event.get():
+            if ev.type == pygame.QUIT:
+                game.should_quit = True
+
+        Input.update()
+
+        if Input.is_pressed_once(pygame.K_z):
+            if Menu.is_open("ingame_pause"):
+                Menu.close_menu("ingame_pause")
+            else:
+                Menu.open_menu("ingame_pause")
+
+        window.fill("black")
+        ButtonManager.update()
+        display.update()
+
+        clock.tick_busy_loop(70)
+
+
+if __name__ == "__main__":
+    # button_example()
+    menu_example()
