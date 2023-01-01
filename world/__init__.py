28dc43d85a07a3a2e54e534955d2510a3f611a6e
tchauvel <tom.chauvel@etudiant.univ-rennes1.fr>
2023-01-01 03:30:55 +0100

    saut à améliorer, mais ça marche

--- world/__init__.py
+++ world/__init__.py
@@ -28,7 +28,7 @@ class World:
 
         self.deserialize(GameState.save["data"]["last_map"])
         position: tuple[int, int] = GameState.save["data"]["player"]["position"]
-        self.player = Player(v2(position[0], position[1])*GameConfig.BLOCK_SIZE, 0.95*GameConfig.BLOCK_SIZE*v2(1, 1))
+        self.player = Player(v2(position[0], position[1])*GameConfig.BLOCK_SIZE, 0.95*GameConfig.BLOCK_SIZE*v2(1, 1),5)
         self.camera: Camera = Camera(self.player)
 
     deserialize = deserialize
