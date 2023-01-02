5a5a76cea6ceda55567bf27fb8f7c2d0a8c6e330
vianney-herve <105049233+vianney-herve@users.noreply.github.com>
2023-01-02 11:33:30 +0100

    ajout de coolidable dans MapElement

--- assets/scripts/environment.py
+++ assets/scripts/environment.py
@@ -11,15 +11,22 @@ from assets.scripts.lightsource import LightSource
 
 class MapElement(Renderable):
     """Élément nécessaire pour construire le niveau du jeu\n
-    Définie une méthode static `create` et un `mask` de collision pour chaque entité appartenant au décors
+    Définie une méthode static `create` et un `mask` de collision pour chaque entité appartenant au décors.\n
     - Hérite de Renderable <- GameObject
     """
     def create(coords: tuple[int, int], id: int, state: int, uuid: int):
         return MapElement(coords, Palette.get_texture(id, state))
 
-    def __init__(self, coords: tuple[int, int], texture: pg.Surface) -> None:
+    def __init__(self, coords: tuple[int, int], texture: pg.Surface, collidable: bool = True) -> None:
+        """MapElement(constructeur)
+
+        Paramètres:
+            coords (tuple[int, int]): la position dans la matrice du niveau.
+            texture (pg.Surface): une référence vers la texture à afficher.
+            collidable (bool, optional): Si Vrai alors le `mask` correspond à la texture, sinon le `mask` est vide. Défaut à True.
+        """
         Renderable.__init__(self, pg.Vector2(coords[1], coords[0]) * GameConfig.BLOCK_SIZE, texture)
-        self.mask: pg.Mask = pg.mask.from_surface(self.texture)
+        self.mask: pg.Mask = pg.mask.from_surface(self.texture) if collidable else pg.Mask((0, 0))
 
 class Platform(MapElement):
     """Élément représentant une platforme dans le niveau 
@@ -42,7 +49,7 @@ class Lamp(MapElement, LightSource):
         return Lamp(coords, Palette.get_texture(id, state))
 
     def __init__(self, coords: tuple[int, int], texture: pg.Surface) -> None:
-        MapElement.__init__(self, coords, texture)
+        MapElement.__init__(self, coords, texture, False)
         LightSource.__init__(self)
 
     @property
