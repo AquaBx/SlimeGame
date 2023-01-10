fa8c6f80bd65f4161bd32769d2c103005fb816c7
Malo | Astri <74017420+Astri2@users.noreply.github.com>
2023-01-10 21:37:42 +0000

    Mort & RPC & respawn stuff

--- world/serializer.py
+++ world/serializer.py
@@ -7,8 +7,9 @@ import pygame as pg
 from pygame import transform, image,Vector2 as v2
 
 from config import GameConfig
+from assets.scripts.environment import MapElement
 
-def deserialize(world, file: str) -> None:
+def deserialize(world, file: str) -> tuple[np.ndarray[(None, MapElement)], pg.Surface]:
         # on ouvre le fichier dans le dossier .../slime_game/assets/maps/
         f = open(f"assets/maps/{file}.map", "rb")
 
@@ -24,18 +25,19 @@ def deserialize(world, file: str) -> None:
         # on lit ensuite les dimensions de la grille
         (grid_rows, grid_columns) = struct.unpack("@bb", f.read(2))
         environment: pg.Surface = pg.Surface(v2(grid_columns, grid_rows)*GameConfig.BLOCK_SIZE, flags=pg.SRCALPHA).convert_alpha()
-        world.blocks = np.full((grid_rows, grid_columns), None)
+        blocks = np.full((grid_rows, grid_columns), None)
         for i in range(grid_rows):
             for j in range(grid_columns):
                 (id, state, uuid) = struct.unpack("@bbh", f.read(4))
 
                 if id == -1: continue
-                world.blocks[i, j] = table[id].script.create((i, j), id, state, uuid)
-                environment.blit(world.blocks[i, j].texture, world.blocks[i, j].position)
+                blocks[i, j] = table[id].script.create((i, j), id, state, uuid)
+                environment.blit(blocks[i, j].texture, blocks[i, j].position)
 
         # enfin on lit le background de la map
         background_id: int = struct.unpack("@h", f.read(2))[0]
 
-        world.background: pg.Surface = transform.scale(image.load(ASSETS[background_id].path).convert(), environment.get_size())
-        world.background.blit(environment, (0,0))
+        background: pg.Surface = transform.scale(image.load(ASSETS[background_id].path).convert(), environment.get_size())
+        background.blit(environment, (0,0))
         f.close()
+        return (blocks, background)
